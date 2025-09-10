1d5790029276a48b6af129efeddf987031f3f713
anuragmishra22071999-sudo <anuragmishra22071999@gmail.com>
2025-09-10 10:27:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const axios = require("axios");
+const log = require("npmlog");
 
+function start() {
+  log.info("FCA", "🚀 FCA-Anurag is now running!");
+
+  // Example API request
+  axios.get("https://api.github.com/users/octocat")
+    .then(res => {
+      log.info("API", "Github user: " + res.data.login);
+    })
+    .catch(err => {
+      log.error("API", err.message);
+    });
+}
+
+module.exports = {
+  start
+};
